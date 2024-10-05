576ef5940055c8bcfbd13b91146979fe9abd2d87
AbdallaHawwas <58154556+AbdallaHawwas@users.noreply.github.com>
2024-10-06 00:54:02 +0300

    sync with live

--- resources/views/payment/paymobPayementResponse.blade.php
+++ resources/views/payment/paymobPayementResponse.blade.php
@@ -40,7 +40,7 @@
             <img src="/assets/images/power logo.svg" style="width: 300px;" alt="Powerfull Logo">
           </div>
           {{-- Return button --}}
-          <div class="m-auto w-25 close">
+          <div class="m-auto w-25 closePowerfullIframe">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="130" height="130" viewBox="0 0 24 24" stroke-width="2.5" stroke="#ec711c" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
               <path d="M15 6l-6 6l6 6" />
