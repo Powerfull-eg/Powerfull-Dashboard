f127f11cea44319195cd6ccfc68662a32f906c89
AbdallaHawwas <abdallahawwas9@gmail.com>
2024-10-17 11:24:46 +0300

    add fawry integrations

--- app/Http/Controllers/Dashboard/PaymentController.php
+++ app/Http/Controllers/Dashboard/PaymentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Http\Controllers\Api\FawryPayController;
+use App\Http\Controllers\Api\PaymobController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
